400a99a8a0d305e2995b244fa2ce25257efa1a6b
jimmy lee <ele1325@hotmail.com>
2024-12-01 23:33:27 +0800

    1201 jimmy use Qml/QtQuick to replace QtWidgets

--- pyqt5/main.py
+++ pyqt5/main.py
@@ -1,31 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
-
-class HelloWorldApp(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        # 設定視窗大小和標題
-        self.setWindowTitle('Hello World App')
-        self.setGeometry(100, 100, 300, 200)
-
-        # 創建按鈕
-        self.button = QPushButton('Hello, World!', self)
-        self.button.clicked.connect(self.show_message)
-        self.button.resize(self.button.sizeHint())
-        self.button.move(100, 80)
-
-    def show_message(self):
-        # 顯示訊息框
-        QMessageBox.information(self, 'Message', 'Hello, World!')
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlApplicationEngine
 
 def main():
-    app = QApplication(sys.argv)
-    ex = HelloWorldApp()
-    ex.show()
+    app = QGuiApplication(sys.argv)
+    
+    # 使用 QQmlApplicationEngine 加載 QML 文件
+    engine = QQmlApplicationEngine()
+    engine.load(QUrl("main.qml"))
+    
+    if not engine.rootObjects():
+        sys.exit(-1)
     sys.exit(app.exec_())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
